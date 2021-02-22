598ade84c838e0a4f1145daf33e9c8a5694e1f5f
Larazzia <sigismeau.clement@gmail.com>
2021-02-22 13:25:30 +0100

    connexion à la bdd

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -4,4 +4,11 @@ $dbName = 'composer_exam';
 $dbUser = 'root';
 $dbPass = ''; // root si vous utilisez un Mac avec MAMP
 $dbHost = 'localhost'; // ou 127.0.0.1, en cas de soucis (mais les deux devraient être identiques)
-$dbDsn = 'mysql:dbname='.$dbName.';host='.$dbHost;
+$dbDsn = 'mysql:dbname=' . $dbName . ';host=' . $dbHost;
+
+// Connexion à la base de données
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=composer_exam;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
